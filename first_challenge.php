8126518fe111b7dcad8811f70c79c3fbb743f734
root <root@localhost.localdomain>
2021-09-12 18:52:40 +0000

    first challenge

---
+++ first_challenge.php
@@ -0,0 +1,10 @@
+<?php 
+    $name = 'Teresa';  // Heading
+    $city = 'NYC';
+    $movie = 'Star Wars'; // Italics
+    $friend = 'George';
+    $candy = 'Sour Patch Kids';
+
+    echo "<h1> $name </h1> lives in $city with her friend $friend eat $candy whiles watching <i>$movie</i>";
+
+?>
